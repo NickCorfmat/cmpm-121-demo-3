9d77eca35d890ddee881a739dbc07269efe44b7e
Nick Corfmat <ncorfmat@ucsc.edu>
2024-11-05 18:30:46 -0800

    ui update + initial cache spawn

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,90 @@
-// todo
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+import luck from "./luck.ts";
+
+// Classroom location (as identified on Google Maps)
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// Create the map html element
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: true,
+  scrollWheelZoom: true,
+});
+
+interface Coin {
+  cell: string;
+  serial: string;
+}
+
+interface Cache {
+  coins: Coin[];
+}
+
+interface Cell {
+  i: number;
+  j: number;
+}
+
+// Add a tile layer to the map
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+// Add a player marker to the map
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindPopup("Hello, fellow traveler!");
+playerMarker.addTo(map);
+
+// Display the player's points
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "inventory:";
+
+// Add caches to the map by cell numbers
+function spawnCache(i: number, j: number) {
+  const origin = OAKES_CLASSROOM;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  // Add a rectangle to the map to represent the cache
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+}
+
+// Look around the player's neighborhood for caches to spawn
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    // If location i,j is lucky enough, spawn a cache!
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
+
+// // temporary fix to Vite's asset static handling. Code from akhalim, https://github.com/akhalim1/cmpm-121-demo-3/blob/main/src/main.ts
+// function _resolveAssetPath(relativePath: string): string {
+//   return import.meta.resolve(`../public/${relativePath}`);
+// }
