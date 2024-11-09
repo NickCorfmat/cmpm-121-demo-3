b87bec096949111968a6981b2793298be6ac8558
Nick Corfmat <ncorfmat@ucsc.edu>
2024-11-08 18:44:52 -0800

    added comments

--- src/board.ts
+++ src/board.ts
@@ -1,5 +1,6 @@
 import leaflet from "leaflet";
 
+// Import Cell interface defined in main.ts
 import { Cell } from "./main.ts";
 
 // Board flyweight factory
@@ -19,6 +20,7 @@ export class Board {
     const { i, j } = cell;
     const key = [i, j].toString();
 
+    // check if instance already exists, if not, create new Cell
     if (!this.knownCells.has(key)) {
       this.knownCells.set(key, cell);
     }
@@ -27,6 +29,7 @@ export class Board {
   }
 
   getCellForPoint(point: leaflet.LatLng): Cell {
+    // return grid cells in global coordinate system (anchored at 0N 0E)
     return this.getCanonicalCell({
       i: Math.trunc(point.lat / this.tileWidth),
       j: Math.trunc(point.lng / this.tileWidth),
@@ -36,16 +39,18 @@ export class Board {
   getCellBounds(cell: Cell): leaflet.LatLngBounds {
     const { i, j } = cell;
 
-    return leaflet.latLngBounds([
-      [i * this.tileWidth, j * this.tileWidth],
-      [(i + 1) * this.tileWidth, (j + 1) * this.tileWidth],
-    ]);
+    // compute the corner lat/lng coordinates of a cell on the leaflet map
+    const southwest = [i * this.tileWidth, j * this.tileWidth];
+    const northeast = [(i + 1) * this.tileWidth, (j + 1) * this.tileWidth];
+
+    return leaflet.latLngBounds([southwest, northeast]);
   }
 
   getCellsNearPoint(point: leaflet.LatLng): Cell[] {
     const resultCells: Cell[] = [];
     const originCell = this.getCellForPoint(point);
 
+    // Retrieve all nearby cells within a certain radius of the origin
     for (
       let x = -this.tileVisibilityRadius;
       x <= this.tileVisibilityRadius;
